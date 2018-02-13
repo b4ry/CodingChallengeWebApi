4e542b5e852f7928849c10efc8b9120473be98b2
b4ry <barry90@interia.pl>
2018-02-13 16:18:44 +1000

    - initial version of ModelValidationFilter

---
+++ KnockKnockReadifyChallenge/KnockKnockReadifyChallenge.Middlewares/Filters/ModelValidationFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace KnockKnockReadifyChallenge.Middlewares.Filters
+{
+    public class ModelValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                throw new Exception("Invalid request.");
+            }
+        }
+    }
+}
