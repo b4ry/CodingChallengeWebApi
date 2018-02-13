b32f75f75aef45a4b2bbe896de7d509c5e4b46dd
b4ry <barry90@interia.pl>
2018-02-13 15:47:05 +1000

    - configured Swagger,
    - refactoring,
    - some changes in launchSettings

--- KnockKnockReadifyChallenge/KnockKnockReadifyChallenge/Program.cs
+++ KnockKnockReadifyChallenge/KnockKnockReadifyChallenge/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace KnockKnockReadifyChallenge
+namespace KnockKnockReadifyChallenge.Api
 {
     public class Program
     {
--- KnockKnockReadifyChallenge/KnockKnockReadifyChallenge/Startup.cs
+++ KnockKnockReadifyChallenge/KnockKnockReadifyChallenge/Startup.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
 
-namespace KnockKnockReadifyChallenge
+namespace KnockKnockReadifyChallenge.Api
 {
     public class Startup
     {
@@ -18,6 +21,26 @@ namespace KnockKnockReadifyChallenge
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new Info
+                {
+                    Title = "KnockKnock.Web",
+                    Version = "v1.0",
+                    Description = "API for Knock Knock Readify challenge",
+                    Contact = new Contact
+                    {
+                        Name = "Lukasz Barczak",
+                        Email = "luka.barczak@gmail.com",
+                        Url = $"https://github.com/b4ry"
+                    }
+                });
+
+                var basePath = AppContext.BaseDirectory;
+                var xmlPath = Path.Combine(basePath, "KnockKnockReadifyChallenge.xml");
+                setup.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +50,12 @@ namespace KnockKnockReadifyChallenge
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnockKnock.Web V1.0");
+            });
 
             app.UseMvc();
         }
